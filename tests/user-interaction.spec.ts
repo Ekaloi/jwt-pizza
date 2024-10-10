ae9af7367eb0380b0db3edcd529f543bb0545cd5
ethankaloi <ekaloi123@gmail.com>
2024-10-10 11:48:38 -0600

    Remoevd config from docs mock

--- tests/user-interaction.spec.ts
+++ tests/user-interaction.spec.ts
@@ -255,10 +255,6 @@ test('home page', async ({ page }) => {
             },
           },
         ],
-        config: {
-          factory: "https://pizza-factory.cs329.click",
-          db: "127.0.0.1",
-        },
       };
       expect(route.request().method()).toBe('GET');
       await route.fulfill({ json: docsRes });
