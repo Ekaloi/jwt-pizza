e001da6b96015df5411ad933c2799bab02889cd5
ethankaloi <ekaloi123@gmail.com>
2024-10-10 11:37:16 -0600

    Please work

--- tests/user-interaction.spec.ts
+++ tests/user-interaction.spec.ts
@@ -1,10 +1,281 @@
 import { test, expect } from 'playwright-test-coverage';
 
 test('home page', async ({ page }) => {
-  await page.goto('/');
+  await page.route('*/**/api/docs', async (route) => {
+    if (route.request().method() === 'PUT') {
+      const docsRes = {
+        version: "20240926.195142",
+        endpoints: [
+          {
+            method: "POST",
+            path: "/api/auth",
+            description: "Register a new user",
+            example:
+              'curl -X POST localhost:3000/api/auth -d \'{"name":"pizza diner", "email":"d@jwt.com", "password":"diner"}\' -H \'Content-Type: application/json\'',
+            response: {
+              user: {
+                id: 2,
+                name: "pizza diner",
+                email: "d@jwt.com",
+                roles: [
+                  {
+                    role: "diner",
+                  },
+                ],
+              },
+              token: "tttttt",
+            },
+          },
+          {
+            method: "PUT",
+            path: "/api/auth",
+            description: "Login existing user",
+            example:
+              'curl -X PUT localhost:3000/api/auth -d \'{"email":"a@jwt.com", "password":"admin"}\' -H \'Content-Type: application/json\'',
+            response: {
+              user: {
+                id: 1,
+                name: "常用名字",
+                email: "a@jwt.com",
+                roles: [
+                  {
+                    role: "admin",
+                  },
+                ],
+              },
+              token: "tttttt",
+            },
+          },
+          {
+            method: "PUT",
+            path: "/api/auth/:userId",
+            requiresAuth: true,
+            description: "Update user",
+            example:
+              'curl -X PUT localhost:3000/api/auth/1 -d \'{"email":"a@jwt.com", "password":"admin"}\' -H \'Content-Type: application/json\' -H \'Authorization: Bearer tttttt\'',
+            response: {
+              id: 1,
+              name: "常用名字",
+              email: "a@jwt.com",
+              roles: [
+                {
+                  role: "admin",
+                },
+              ],
+            },
+          },
+          {
+            method: "DELETE",
+            path: "/api/auth",
+            requiresAuth: true,
+            description: "Logout a user",
+            example: "curl -X DELETE localhost:3000/api/auth -H 'Authorization: Bearer tttttt'",
+            response: {
+              message: "logout successful",
+            },
+          },
+          {
+            method: "GET",
+            path: "/api/order/menu",
+            description: "Get the pizza menu",
+            example: "curl localhost:3000/api/order/menu",
+            response: [
+              {
+                id: 1,
+                title: "Veggie",
+                image: "pizza1.png",
+                price: 0.0038,
+                description: "A garden of delight",
+              },
+            ],
+          },
+          {
+            method: "PUT",
+            path: "/api/order/menu",
+            requiresAuth: true,
+            description: "Add an item to the menu",
+            example:
+              'curl -X PUT localhost:3000/api/order/menu -H \'Content-Type: application/json\' -d \'{ "title":"Student", "description": "No topping, no sauce, just carbs", "image":"pizza9.png", "price": 0.0001 }\'  -H \'Authorization: Bearer tttttt\'',
+            response: [
+              {
+                id: 1,
+                title: "Student",
+                description: "No topping, no sauce, just carbs",
+                image: "pizza9.png",
+                price: 0.0001,
+              },
+            ],
+          },
+          {
+            method: "GET",
+            path: "/api/order",
+            requiresAuth: true,
+            description: "Get the orders for the authenticated user",
+            example: "curl -X GET localhost:3000/api/order  -H 'Authorization: Bearer tttttt'",
+            response: {
+              dinerId: 4,
+              orders: [
+                {
+                  id: 1,
+                  franchiseId: 1,
+                  storeId: 1,
+                  date: "2024-06-05T05:14:40.000Z",
+                  items: [
+                    {
+                      id: 1,
+                      menuId: 1,
+                      description: "Veggie",
+                      price: 0.05,
+                    },
+                  ],
+                },
+              ],
+              page: 1,
+            },
+          },
+          {
+            method: "POST",
+            path: "/api/order",
+            requiresAuth: true,
+            description: "Create a order for the authenticated user",
+            example:
+              'curl -X POST localhost:3000/api/order -H \'Content-Type: application/json\' -d \'{"franchiseId": 1, "storeId":1, "items":[{ "menuId": 1, "description": "Veggie", "price": 0.05 }]}\'  -H \'Authorization: Bearer tttttt\'',
+            response: {
+              order: {
+                franchiseId: 1,
+                storeId: 1,
+                items: [
+                  {
+                    menuId: 1,
+                    description: "Veggie",
+                    price: 0.05,
+                  },
+                ],
+                id: 1,
+              },
+              jwt: "1111111111",
+            },
+          },
+          {
+            method: "GET",
+            path: "/api/franchise",
+            description: "List all the franchises",
+            example: "curl localhost:3000/api/franchise",
+            response: [
+              {
+                id: 1,
+                name: "pizzaPocket",
+                stores: [
+                  {
+                    id: 1,
+                    name: "SLC",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            method: "GET",
+            path: "/api/franchise/:userId",
+            requiresAuth: true,
+            description: "List a user's franchises",
+            example: "curl localhost:3000/api/franchise/4  -H 'Authorization: Bearer tttttt'",
+            response: [
+              {
+                id: 2,
+                name: "pizzaPocket",
+                admins: [
+                  {
+                    id: 4,
+                    name: "pizza franchisee",
+                    email: "f@jwt.com",
+                  },
+                ],
+                stores: [
+                  {
+                    id: 4,
+                    name: "SLC",
+                    totalRevenue: 0,
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            method: "POST",
+            path: "/api/franchise",
+            requiresAuth: true,
+            description: "Create a new franchise",
+            example:
+              'curl -X POST localhost:3000/api/franchise -H \'Content-Type: application/json\' -H \'Authorization: Bearer tttttt\' -d \'{"name": "pizzaPocket", "admins": [{"email": "f@jwt.com"}]}\'',
+            response: {
+              name: "pizzaPocket",
+              admins: [
+                {
+                  email: "f@jwt.com",
+                  id: 4,
+                  name: "pizza franchisee",
+                },
+              ],
+              id: 1,
+            },
+          },
+          {
+            method: "DELETE",
+            path: "/api/franchise/:franchiseId",
+            requiresAuth: true,
+            description: "Delete a franchises",
+            example:
+              "curl -X DELETE localhost:3000/api/franchise/1 -H 'Authorization: Bearer tttttt'",
+            response: {
+              message: "franchise deleted",
+            },
+          },
+          {
+            method: "POST",
+            path: "/api/franchise/:franchiseId/store",
+            requiresAuth: true,
+            description: "Create a new franchise store",
+            example:
+              "curl -X POST localhost:3000/api/franchise/1/store -H 'Content-Type: application/json' -d '{\"franchiseId\": 1, \"name\":\"SLC\"}' -H 'Authorization: Bearer tttttt'",
+            response: {
+              id: 1,
+              franchiseId: 1,
+              name: "SLC",
+            },
+          },
+          {
+            method: "DELETE",
+            path: "/api/franchise/:franchiseId/store/:storeId",
+            requiresAuth: true,
+            description: "Delete a store",
+            example:
+              "curl -X DELETE localhost:3000/api/franchise/1/store/1  -H 'Authorization: Bearer tttttt'",
+            response: {
+              message: "store deleted",
+            },
+          },
+        ],
+        config: {
+          factory: "https://pizza-factory.cs329.click",
+          db: "127.0.0.1",
+        },
+      };
+      expect(route.request().method()).toBe('Get');
+      await route.fulfill({ json: docsRes });
+    } else{
+      route.continue;
+    }});
 
+  await page.goto('/');
   expect(await page.title()).toBe('JWT Pizza');
+
+  await page.goto('/docs');
+  expect(await page.getByText('JWT Pizza API')).toBeVisible();
+
 });
+
+
 
 
 
@@ -52,7 +323,6 @@ test('buy pizza with login', async ({ page }) => {
       expect(await page.getByText('Mama Rucci, my my')).toBeVisible();
       await page.getByRole('link', { name: 'home' }).click();
       expect(await page.getByText('The web\'s best pizza', { exact: true })).toBeVisible();
-      await page.goto('/docs');
       await page.goto('/about');
       await page.goto('/logout');
      
